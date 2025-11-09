d6aefeaf26c9c9edb6835a2a2ddc9243554c35b3
Olga <olga.chitembo@metropolia.fi>
2025-11-09 21:17:57 +0200

    fe-react-activity

---
+++ Week4/fe-react-activity2/forms-intro-lab/src/ContactUs.jsx
@@ -0,0 +1,89 @@
+// src/ContactUs.jsx
+import { useState } from "react";
+
+function ContactUs() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+    const [phoneType, setPhoneType] = useState("");
+    const [comments, setComments] = useState("");
+    const onSubmit = e => {
+    // Prevent the default form behavior so the page doesn't reload.
+    e.preventDefault();
+    const contactUsInformation = {
+      name,
+      email,
+      phone,
+      phoneType,
+      comments,
+      submittedOn: new Date()
+    };
+
+    console.log(contactUsInformation);
+    // Reset the form state.
+    setName('');
+    setEmail('');
+    setPhone('');
+    setPhoneType('');
+    setComments('');
+  };
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <select
+            name="phoneType"
+            onChange={(e) => setPhoneType(e.target.value)}
+            value={phoneType}
+          >
+            <option value="" disabled>
+              Select a phone type...
+            </option>
+            <option value="home">Home</option>
+            <option value="work">Work</option>
+            <option value="mobile">Mobile</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="comments">Comments:</label>
+          <textarea
+            id="comments"
+            name="comments"
+            onChange={(e) => setComments(e.target.value)}
+            value={comments}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactUs;
