0aeceabf1be458a561bd37f03bbe3b431caab135
Olga <olga.chitembo@metropolia.fi>
2025-11-19 10:29:37 +0200

    JWT activity

---
+++ Week5/JWT/be-jwt-activity/jwt_lab.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+
+// Function to create and sign a JWT
+function createJWT() {
+  const payload = {
+    userId: 123,
+    username: 'exampleUser'
+  };
+  const secretKey = '1763386329'; // Replace with your secret key
+
+  // Sign the JWT with the payload and secret key
+  const token = jwt.sign(payload, secretKey);
+
+  console.log('JWT Token:', token);
+}
+
+// Call the function to create and sign a JWT
+createJWT();
+
+// Function to verify a JWT
+function verifyJWT(token) {
+  const secretKey = '1763386329'; // Replace with your secret key
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.error('JWT Verification Failed:', err.message);
+    } else {
+      console.log('JWT Verified. Decoded:', decoded);
+    }
+  });
+}
+
+// Replace 'yourTokenHere' with a JWT token you generated in Step 4
+const jwtTokenToVerify = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywidXNlcm5hbWUiOiJleGFtcGxlVXNlciIsImlhdCI6MTc2MzM4NjIwMn0.nVHOKyFqzV0Qrs5GcnFI6EM2LTfy8Q9_rZ7f51VywvQ';
+
+// Call the function to verify the JWT
+verifyJWT(jwtTokenToVerify);
+
+// Function to decode a JWT
+function decodeJWT(token) {
+  const decoded = jwt.decode(token);
+
+  console.log('Decoded JWT:', decoded);
+}
+
+// Replace 'yourTokenHere' with a JWT token you generated in Step 4
+const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywidXNlcm5hbWUiOiJleGFtcGxlVXNlciIsImlhdCI6MTc2MzM4NjMyOX0.uJ32PAPq_q5UCJ93xEAi87yVgIbjr43Za2JFJlFE5e4';
+
+// Call the function to decode the JWT
+decodeJWT(jwtToken);
